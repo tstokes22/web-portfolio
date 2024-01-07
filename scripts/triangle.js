4949226d94705632b6bdbec70a09dca937ae0990
tstokes22 <tbstokes22@gmail.com>
2024-01-07 14:00:42 -0500

    modified:   index.html 	deleted:    scripts/chessboard.js 	deleted:    scripts/fizzbuzz.js 	deleted:    scripts/triangle.js 	modified:   styles/styles.css 	deleted:    task.html

--- scripts/triangle.js
+++
@@ -1,8 +0,0 @@
-let count = 7;
-let triangle = "";
-document.getElementById("addTaskBtn").addEventListener("click", () => {
-    for(let i = 0; count > i; i++){
-        triangle += "#";
-        console.log(triangle);
-    }
-});
